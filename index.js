1fe3391172819f2291ef0604da39b7140fbafe68
Jan Monschke <jan.monschke@gmail.com>
2015-05-25 17:00:21 +0200

    first version

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var CouchDBDataAdapter;
+var CouchDBDataAdapter = function(database){
+  if(!database){ throw new Error('Need to specify a database'); }
+  this.database = database;
+};
+
+CouchDBDataAdapter.prototype.getData = function(id, callback){
+  this.database.get(id, callback);
+};
+
+CouchDBDataAdapter.prototype.storeData = function(id, data, callback){
+  // we override the document in any way
+  delete data._rev;
+
+  this.database.get(id, function(err, doc){
+    // return with error
+    if(err && callback){ return callback(err); }
+
+    // override the document with the current state
+    this.database.save(id, doc._rev, callback);
+  }.bind(this));
+};
 
 module.exports = CouchDBDataAdapter;
